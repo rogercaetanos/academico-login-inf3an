705ff0fd590a23df2e6c9908e397a906e0449c4b
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2024-07-30 22:33:52 -0300

    Base necessária para iniciar o security

---
+++ src/main/java/com/itb/lip2/academicologininf3an/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.itb.lip2.academicologininf3an.security;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
