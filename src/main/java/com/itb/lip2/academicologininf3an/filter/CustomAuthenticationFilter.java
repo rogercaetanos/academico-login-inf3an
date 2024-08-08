72ff37aa785c980d80fad058d43c828f7175b144
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2024-08-07 21:21:44 -0300

    Filtros de autenticação e autorização parcialmente concluídos

--- src/main/java/com/itb/lip2/academicologininf3an/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/itb/lip2/academicologininf3an/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.itb.lip2.academicologininf3an.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itb.lip2.academicologininf3an.model.Usuario;
 import com.itb.lip2.academicologininf3an.repository.UsuarioRepository;
 import com.itb.lip2.academicologininf3an.service.UsuarioService;
@@ -9,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,6 +20,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 // Filtro responsável em autenticar o usuário
@@ -49,6 +56,12 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         String access_token = JWT.create().withSubject(usuario.getId().toString())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
 
 
     }
